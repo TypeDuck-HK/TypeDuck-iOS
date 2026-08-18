CanCLID/Cantoboard

Fix keyboard top and bottom paddings in iPhone landscape orientation
The top and bottom insets are each 2 pixels wider than iOS native,
causing the keyboard to appear slightly flatter.

CantoboardFramework/Keyboard/View/Keypad/KeypadView.swift
@@ -220,7 +220,7 @@ class KeypadView: UIView, BaseKeyboardView {
     }
     
     private func layoutButtons(_ buttons: [[KeypadButton]], initialX: CGFloat, layoutConstants: LayoutConstants) {
-        var x: CGFloat = initialX, y: CGFloat = LayoutConstants.keyboardViewTopInset + layoutConstants.autoCompleteBarHeight
+        var x: CGFloat = initialX, y: CGFloat = layoutConstants.keyboardViewInsets.top + layoutConstants.autoCompleteBarHeight
         
         for row in buttons {
             x = initialX

TypeDuck-HK/TypeDuck-iOS

(cherry picked from commit 0c2a8eda73f23a1b907fb18410e09381c8810162)

CantoboardFramework/Keyboard/View/Keypad/KeypadView.swift
@@ -218,7 +218,7 @@ class KeypadView: UIView, BaseKeyboardView {
     
     private func layoutButtons(_ buttons: [[KeypadButton]], initialX: CGFloat, layoutConstants: LayoutConstants) {
         guard let candidatePaneView = candidatePaneView else { return }
-        var x: CGFloat = initialX, y: CGFloat = LayoutConstants.keyboardViewTopInset + candidatePaneView.rowHeight
+        var x: CGFloat = initialX, y: CGFloat = layoutConstants.keyboardViewInsets.top + candidatePaneView.rowHeight
         
         for row in buttons {
             x = initialX